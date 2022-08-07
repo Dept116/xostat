7dee542f22a431192cbf002f58e310c4062e39fb
Morgan Hall <morg.hall@gmail.com>
2022-08-07 17:25:14 -0400

    item class

---
+++ xostat-lambda-api/classes/item.py
@@ -0,0 +1,9 @@
+class item:
+    def __init__(self):
+        self.local_name = ""
+        self.name = ""
+        self.category = ""
+        self.type = ""
+        self.crossoutdb_item = 0
+        self.image = ""
+        self.image_location = ""
